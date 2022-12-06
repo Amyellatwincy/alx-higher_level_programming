6dad581a20c64ef2d4c08586301f2e2b43435a45
Amyellatwincy <emmanuellaokonkwo.oe@gmail.com>
2022-12-06 14:56:23 +0000

    task

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-for i in range (97, 123):
-# if i match code for 'q' and 'e' skip it
-	if i == 101 or i == 113:
-		continue
-
-	print("{:c}".format(i), end='')
+for i in range(ord('a'), ord('z') + 1):
+    if chr(i) != 'e' and chr(i) != 'q':
+        print('{:c}'.format(i), end='')
