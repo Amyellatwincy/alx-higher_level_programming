6a6e1b0c4bc05eff4708e40184e357b272e2cdad
Amyellatwincy <emmanuellaokonkwo.oe@gmail.com>
2022-12-09 13:17:38 +0000

    task reviewed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
-
-
 if __name__ == "__main__":
-
     """Print the number of and list of arguments."""
     import sys
 
@@ -14,5 +11,5 @@ if __name__ == "__main__":
 	print("1 argument:")
     else:
 	print("{} arguments:".format(count))
-    for 1 in range(count):
+    for i in range(count):
 	print("{}: {}".format(i + 1, sys.argv[i +1]))
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# This code should not run when this file is imported
 
 if __name__ == "__main__":
     """Print all names defined by hidden_4 module."""
@@ -7,5 +6,5 @@ if __name__ == "__main__":
 
     names = dir(hidden_4)
     for name in names:
-	if name[:2} != "__":
-	    print(name)
+        if name[:2] != "__":
+            print(name)
