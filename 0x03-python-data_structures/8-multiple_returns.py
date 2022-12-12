b850537f979b721d0a2ceaab52af7f8eca8f3bf2
Amyellatwincy <emmanuellaokonkwo.oe@gmail.com>
2022-12-12 09:22:20 +0000

    task fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    str_len, first_char = len(sentence), sentence[0]
-    return (str_len, first_char)
+    if sentence != '':
+        first_char = sentence[0]
+    else:
+        first_char = None
+    return (len(sentence), first_char)
