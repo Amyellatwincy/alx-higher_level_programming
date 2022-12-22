30b597a96a8c6edea610327d62f5d5bea7889618
Amyellatwincy <emmanuellaokonkwo.oe@gmail.com>
2022-12-22 02:04:23 -0800

    task all

---
+++ 0-add.py
@@ -0,0 +1,8 @@
+from task import add
+
+a = 1
+b = 2
+
+# This code should not run when this file is imported
+if __name__ == "__main__":
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
---
+++ README.md
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+this is my readme for this Alx high level programming
